344df075d29948b1acc8be5ede1d79498973a1ca
anp3099 <56688249+anp3099@users.noreply.github.com>
2025-08-10 17:48:43 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import env from "./lib/validateEnv";
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { validateClientEnv } from './lib/validateEnv'
+import { validateClientEnv } from './lib/validateEnv';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-validateClientEnv()
+// Validate required env vars once at startup
+validateClientEnv();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
