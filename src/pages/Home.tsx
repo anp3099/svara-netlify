c16b8acc76f5f2e6a6c4995c29a4060607c23412
anp3099 <56688249+anp3099@users.noreply.github.com>
2025-08-11 09:10:27 +0200

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+// src/pages/Home.tsx
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Home</h1>
+      <p>If you see this, the pages/Home import is working.</p>
+    </div>
+  );
+}
